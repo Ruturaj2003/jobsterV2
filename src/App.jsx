d6f04ae57b57d6f5bd78502e44ff1d7ddc0cb77b
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-08-08 18:28:33 +0530

    Create Wrapper For Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react';
 import Landing from './pages/Landing';
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Landing>Dota</Landing>
