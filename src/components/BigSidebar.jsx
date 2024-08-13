b285cacf55b026dd4df8b3fbf1275082f25c4058
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-08-13 19:16:42 +0530

    Implement BigSidebar

--- src/components/BigSidebar.jsx
+++ src/components/BigSidebar.jsx
@@ -1,9 +1,27 @@
+import { useSelector } from 'react-redux';
 import Wrapper from '../assets/wrappers/BigSidebar';
+import Logo from './Logo';
+import NavLinks from './NavLinks';
 
 const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+
   return (
     <Wrapper>
-      <h1>BigSidebar</h1>
+      <div
+        className={
+          !isSidebarOpen
+            ? 'sidebar-container show-sidebar'
+            : 'sidebar-container'
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo></Logo>
+          </header>
+          <NavLinks></NavLinks>
+        </div>
+      </div>
     </Wrapper>
   );
 };
