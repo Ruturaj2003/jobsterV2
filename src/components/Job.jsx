258848d99a9552542325ebfe2ef45ea8a8c198dc
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-08-16 20:47:40 +0530

    Setup and use momentJS

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Job';
 import JobInfo from './JobInfo';
+import moment from 'moment';
 
 const Job = ({
   _id,
@@ -15,7 +16,7 @@ const Job = ({
   status,
 }) => {
   const dispatch = useDispatch();
-  const date = createdAt;
+  const date = moment(createdAt).format('MMM Do YY');
   return (
     <Wrapper>
       <header>
